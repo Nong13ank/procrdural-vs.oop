48b7cd5694a1828ca5aadfd71a94737a9f1e95ff
Nong13ank <jumming1996@gmail.com>
2025-08-05 11:37:42 +0700

    Add files via upload

---
+++ Procdural Programming.py
@@ -0,0 +1,15 @@
+product_list = [] 
+def add_product(product_list):
+    product_name = input("Enter Product Name: ")
+    product_quantity = input("Enter product Quantity:")
+    if product_name in product_list:
+        product_list[product_name] += product_quantity
+    else:
+        product_list[product_name] = product_quantity
+def show_products(product_list):
+    for key in product_list.key():
+        print(key + ":" + str(product_list[key]))
+add_product(product_list,"Snack",10)
+add_product(product_list,"Drink",20)
+add_product(product_list,"Snack",10)
+show_products(product_list)
